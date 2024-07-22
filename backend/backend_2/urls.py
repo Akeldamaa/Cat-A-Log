47e80a0eec6cea35888630ac1e7ffbd648557ccd
JaydenPearlM <168348521+JaydenPearlM@users.noreply.github.com>
2024-07-22 18:49:33 -0400

    Add VS Code files

--- backend/backend_2/urls.py
+++ backend/backend_2/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.views.generic import TemplateView
-from . import views
+from .. import views
 
 urlpatterns = [
     path('upload_image/', views.upload_image, name='upload_image'),
