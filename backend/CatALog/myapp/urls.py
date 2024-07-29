ade626cb9c03c5e01f22bc7e139668a2e824297c
Jonathan <104794912+Akeldamaa@users.noreply.github.com>
2024-07-29 09:53:15 -0500

    moved backend files, added new gitignore

--- backend/CatALog/myapp/urls.py
+++ backend/CatALog/myapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name='index'),  # Example view
+    path('', views.index, name='index'),
+    path('upload/', upload_image, name='upload_image'),  # Example view
 ]
