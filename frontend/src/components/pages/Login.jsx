3c8310e3d81685511d0670b5bde50d757cb402f4
EmeraldVoltron <40584963+EmeraldVoltron@users.noreply.github.com>
2024-07-30 16:56:55 -0500

    Changed file structuring, moved app.jsx contents
    Created new files for the pages that will be used, they are now located in the pages folder. All of the previous app.jsx was moved into the userDashboard.jsx. New App.jsx has a router from react-router-dom to allow navigation between pages.

---
+++ frontend/src/components/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Login.css';
+
+const Login = () => {
+    return(
+            <div className='background'>
+                <h1>Login page</h1>
+            </div>
+    )
+}
+
+export default Login
+
