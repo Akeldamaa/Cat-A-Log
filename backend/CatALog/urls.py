abf8e09e5747c8b0cc4ffcd1feff63be476e826c
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2024-07-29 21:10:08 -0700

    feat: created a new app and set up django rest framework

--- backend/CatALog/urls.py
+++ backend/CatALog/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for CatALog project.
+URL configuration for catalog project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,10 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),  
+    path('', views.index, name='index'),
+    path('api/cards/', include('cards.urls')),  
 ]
