8027e97560cd525ce59a7502f053219eda5d0d37
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2024-08-05 11:54:04 -0700

    feat: changed project folder structure to standard structure

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for catalog project.
+URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -16,12 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 # from django.conf import settings
 # from django.conf.urls.static import static
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
-    path('api/cards/', include('cards.urls')),  
+    path('api/cards/', include('cards.urls')), 
 ]
--- backend/backend/views.py
+++ backend/backend/views.py
@@ -2,7 +2,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-# Create your views here.
 @api_view(['GET'])
 def index(request):
     return Response({'msg': 'Welcome to the Cat-A-Log API!'})
