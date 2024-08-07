a9f741ef0df86830bba0d6280616849732d020e2
EmeraldVoltron <40584963+EmeraldVoltron@users.noreply.github.com>
2024-08-07 17:58:47 -0500

    Added forgot password and signup page content
    added forgot password and signup page content to match the login page styling,
    
    Co-Authored-By: Jonathan <104794912+Akeldamaa@users.noreply.github.com>

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Login.css';
-import LoginForm from './components/LoginForm';
 
 const Login = () => {
     return (
@@ -17,11 +16,13 @@ const Login = () => {
                             <input type="password" placeholder="Password" className="login-input" />
                         </div>
                     </div>
+                    <div className="login-footer">
+                        <a href="forgot-password" className="forgot-password">Forgot Password?</a>
+                    </div>
+                    <button>
+                        Login!
+                    </button>
                 </form>
-                <div className="login-footer">
-                    <a href="forgot-password" className="forgot-password">forgot password?</a>
-                    <a href="signup" className="signup-link">Already a member?</a>
-                </div>
             </div>
         </div>
     );
