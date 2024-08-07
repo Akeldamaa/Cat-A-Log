315d51d7349c290fd37a2079b6e62a110ea2fe83
Jonathan <104794912+Akeldamaa@users.noreply.github.com>
2024-08-07 15:49:21 -0500

    Stylelized Login page

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -3,35 +3,27 @@ import './Login.css';
 
 const Login = () => {
     return (
-        <div className="home-container">
-            <div>
-                <h1>Cat-A-Log</h1>
+        <div className="login-container">
+            <div className="login-card">
+                <h1 className="login-title">Cat-A-Log</h1>
+                <h2 className="login-subtitle">Login</h2>
                 <form>
                     <div className="login-fields">
-                        <div className="username">
-                            <label>Username</label>
-                            <input></input>
+                        <div className="input-container">
+                            <input type="text" placeholder="Username" className="login-input" />
                         </div>
-                        <div className="password">
-                            <label>Password</label>
-                            <input></input>
+                        <div className="input-container">
+                            <input type="password" placeholder="Password" className="login-input" />
                         </div>
                     </div>
                 </form>
-                <div className="forgot-password">
-                    <p><a href="forgot-password">Forgot Password?</a></p>
-                </div>
-                <div className="login-button">
-                    <button>Log in</button>
-                </div>
-                <div>
-                    <button>Signup</button>
+                <div className="login-footer">
+                    <a href="forgot-password" className="forgot-password">forgot password?</a>
+                    <a href="signup" className="signup-link">Already a member?</a>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
 export default Login;
-
-
