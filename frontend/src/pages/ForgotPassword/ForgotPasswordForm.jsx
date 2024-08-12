fe590680053117ae5c1552ffdb42d528d8030ed8
EmeraldVoltron <40584963+EmeraldVoltron@users.noreply.github.com>
2024-08-12 18:39:09 -0500

    Update validation on forgot password and log in form
    Update validation on forgot password and log in form

--- frontend/src/pages/ForgotPassword/ForgotPasswordForm.jsx
+++ frontend/src/pages/ForgotPassword/ForgotPasswordForm.jsx
@@ -1,13 +1,14 @@
 import ReactDOM from "react-dom";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
 const schema = yup
     .object({
         email: yup.string()
-        .email("Email must be valid.")
-        .required("Email cannot be blank."),
+            .email("Email must be valid.")
+            .required("Email cannot be blank."),
     })
     .required()
 
@@ -20,25 +21,45 @@ export default function ForgotPasswordForm() {
         resolver: yupResolver(schema),
     })
 
+    const [errorOrSuccess, setSuccess] = useState({
+        success: false,
+        error: false,
+        neutral: false
+    });
+
     const onSubmit = (data) => {
-        alert(JSON.stringify(data))
+        let validInput = true;
+        if (validInput == true) {
+            alert(JSON.stringify(data))
+            setSuccess({ success: true, error: false, neutral: false })
+        }
+        if (validInput == false) {
+            setSuccess({ success: false, error: true, neutral: false })
+        }
+
     }
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className="forgotPassword-form">
             <div className="forgotpassword-fields">
                 <div>
-                <label htmlFor="email" className="forgotpassword-label">
-                    Email
-                </label>
-                <input {...register("email", {required: true})}
-                    id="email"
-                    className="login-input"
-                    placeholder="Email"
-                />
-                <span className="error-msg">{errors.email?.message}</span>
+                    <label htmlFor="email" className="forgotpassword-label">
+                        Email
+                    </label>
+                    <input {...register("email", { required: true })}
+                        id="email"
+                        className="login-input"
+                        placeholder="Email"
+                    />
+                    <span className="error-msg">{errors.email?.message}</span>
                 </div>
                 
+                <div>
+                    <span className="text-neutral">{errorOrSuccess.neutral && ""}</span>
+                    <span className="text-success">{errorOrSuccess.success && "Email has been sent!"}</span>
+                    <span className="text-error">{errorOrSuccess.error && "Email not found."}</span>
+                </div>
+
                 <div className="login-footer">
                     <a href="login" className="login-link">Back to Login</a>
                     <a href="signup" className="signup-section">Create an Account</a>
--- frontend/src/pages/Login/LoginForm.jsx
+++ frontend/src/pages/Login/LoginForm.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 import "./Login.css";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
@@ -20,8 +21,24 @@ export default function LoginForm() {
         resolver: yupResolver(schema),
     })
 
+    const [errorOrSuccess, setSuccess] = useState({
+        success: false,
+        error: false,
+        neutral: false
+    });
+
+
     const onSubmit = (data) => {
-        alert(JSON.stringify(data))
+        let validInput = true;
+        //Validate if login information is valid
+        if (validInput == true){
+            setSuccess({success: true, error: false, neutral: false})
+            alert(JSON.stringify(data))
+            //Redirect to user dashboard
+        }
+        if (validInput == false){
+            setSuccess({success: false, error: true, neutral: false})
+        }
     }
 
     return (
@@ -48,6 +65,11 @@ export default function LoginForm() {
                         placeholder="Password"
                     />
                     <span className="error-msg">{errors.password?.message}</span>
+                </div>
+                <div>
+                    <span className="text-neutral">{errorOrSuccess.neutral && ""}</span>
+                    <span className="text-success">{errorOrSuccess.success && "Successfully Logged in!"}</span>
+                    <span className="text-error">{errorOrSuccess.error && "Invalid login information"}</span>
                 </div>
                 <div className="login-footer">
                     <a href="forgot-password" className="forgot-password">Forgot Password?</a>
