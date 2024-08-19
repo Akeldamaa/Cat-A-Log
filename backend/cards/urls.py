e0175c9d85491dadbf2440be95cff233dba37090
Jonathan <104794912+Akeldamaa@users.noreply.github.com>
2024-08-19 16:10:18 -0500

    added Dela gothic one as Global font

--- backend/cards/urls.py
+++ backend/cards/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    #path('upload/', upload_image, name='upload_image'),  # Example view
+    path("media/upload/", views.upload_images, name="index"),
 ]
