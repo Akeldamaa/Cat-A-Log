9f8bdb4e30093129045c6048b6f187639dc1da13
EmeraldVoltron <40584963+EmeraldVoltron@users.noreply.github.com>
2024-08-19 16:09:02 -0500

    Delete about us page
    delete about us page (moved to home page)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,6 @@ import Signup from "./pages/Signup/Signup";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import DashBoard from "./pages/Dashboard/Dashboard";
-import About from "./pages/About/About";
 import FeaturedShelter from "./pages/FeaturedShelter/FeaturedShelter";
 import ForgotPassword from "./pages/ForgotPassword/ForgotPassword";
 import Error from "./pages/Error/Error";
@@ -17,7 +16,6 @@ export default function App() {
       <Route path="/" element={<Home />} />
       <Route path="featured-shelter" element={<FeaturedShelter />} />
       <Route path="forgot-password" element={<ForgotPassword />} />
-      <Route path="about" element={<About />} />
       <Route element={<AuthenticatedRoutes />}>
         {/* All authenticated routes (routes that can only be accessed when users are not logged in) are stored here. Remove the signup and login route from here to access them without logging out*/}
         <Route path={"/signup"} element={<Signup />} />
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -12,13 +12,40 @@ export default function Home() {
           <Navbar />
         </div>
         <div className="home-section">
-          <h2>Welcome!</h2>
-          <p>
-            This app allows you to upload images of your cats, analyze their
-            features using AI, customize them with stickers and elements, and
-            even mint them as NFTs on the blockchain. Explore our page, or
-            create an account to start!
-          </p>
+          <div className="welcome-section">
+            <h2>Welcome!</h2>
+            <p>
+              This app allows you to upload images of your cats, analyze their
+              features using AI, customize them with stickers and elements, and
+              even mint them as NFTs on the blockchain. Explore our page, or
+              create an account to start!
+            </p>
+          </div>
+          <div className="mission-statement">
+            <h2>Our Mission</h2>
+            <p>
+
+            </p>
+          </div>
+          <div className="about-us">
+            <h2>About Us</h2>
+            <p>
+
+            </p>
+          </div>
+          <div className="current-abilities">
+            <h2>Current Features</h2>
+            <p>
+
+            </p>
+          </div>
+          <div className="feature-timeline">
+            <h2>Future Feature Timeline</h2>
+            <p>
+              
+            </p>
+          </div>
+
         </div>
       </div>
     </div>
