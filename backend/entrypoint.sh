861148dea65970914b9881c7a53e48fa60e2c99f
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2024-08-24 20:34:37 -0500

    feat: deployed latest app and added configurations for latest deployment

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo "Migrating database..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
-echo "Database migrated"
+# echo "Migrating database..."
+# python manage.py makemigrations --noinput
+# python manage.py migrate --noinput
+# echo "Database migrated"
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 echo "Static files collected"
 
 echo "Starting server..."
-gunicorn backend.wsgi:application --bind 0.0.0.0:8000
+# gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --timeout 200 --workers 3
+gunicorn backend.wsgi:application --bind 0.0.0.0:8000 --workers 2
