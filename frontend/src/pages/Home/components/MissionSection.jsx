d61f74078da0da760acf85d01a8bbab738659310
Moyinoluwa Adelowo <moyinadelowo@gmail.com>
2024-08-26 13:03:03 -0500

    feat deployed tested and updated the apps for presentation

--- frontend/src/pages/Home/components/MissionSection.jsx
+++ frontend/src/pages/Home/components/MissionSection.jsx
@@ -1,32 +1,34 @@
 import { Link } from "react-router-dom";
-import TradingCard from "../../Assets/SavannahCat.jpg"
+import SavannahCat from "../../Assets/SavannahCat.jpg";
+import DomesticCat from "../../Assets/DomesticCat.jpg";
+import MinskinCat from "../../Assets/MinskinCat.jpg";
 
 export default function MissionSection() {
   return (
     <div className="mission-section">
-      <div className="flex-container">
-        <div className="column">
-          <img 
-            className = "tradingCard"
-            src={TradingCard}
-            alt="trading card of Savannah Cat"
-          />
+      <div>
+        <h2>Our Mission</h2>
+      </div>
+      <div>
+        <p>
+          The <strong>Cat-A-Log</strong> app aims to provide a fun and engaging
+          platform for cat enthusiasts to collect, trade, and learn about
+          various cat breeds and their unique attributes. Beyond celebrating the
+          diversity of feline breeds, we have dedicated a{" "}
+          <Link to="/featured-shelter">page</Link> to supporting local animal
+          shelters by promoting adoption and awareness. We aim to improve
+          awareness while offering a fun Cat Trading Card app.
+        </p>
+      </div>
+      <div className="cat-images">
+        <div className="cat cat--1">
+          <img src={DomesticCat} alt="domestic cat" />
         </div>
-        <div className="column">
-          <div>
-            <h2>Our Mission</h2>
-          </div>
-          <div>
-            <p>
-              The <strong>Cat-A-Log</strong> app aims to provide a fun and engaging
-              platform for cat enthusiasts to collect, trade, and learn about
-              various cat breeds and their unique attributes. Beyond celebrating the
-              diversity of feline breeds, we have dedicated a{" "}
-              <Link to="/featured-shelter">page</Link> to supporting local animal
-              shelters by promoting adoption and awareness. We aim to improve
-              awareness while offering a fun Cat Trading Card app.
-            </p>
-          </div>
+        <div className="cat cat--2">
+          <img src={SavannahCat} alt="savannah cat" />
+        </div>
+        <div className="cat cat--3">
+          <img src={MinskinCat} alt="minskin cat" />
         </div>
       </div>
     </div>
